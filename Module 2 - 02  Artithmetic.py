c77739b438f6796ea2568e0bb1205b4d81ec036b
Karim <kwaljee@gmail.com>
2021-05-30 16:32:37 -0400

    Code updated.

--- Module 2 - 02  Artithmetic.py
+++ Module 2 - 02  Artithmetic.py
@@ -6,14 +6,28 @@ print(2*3) # multiplication
 print(2**3) # exponential multiplication
 
 # Slash
-print(6/2) # division with float output
-print(6//2) # division with no float output rounded values
+print()
+print(6/2, " One slash division with float output")
+print(6/4, " One slash division with float output in decimals no rounding")
+print(6//2, "   Two slash division with no float output rounded values")
+print(6//5, "   Two slash division with no float output rounded values")
+print(6.0//4, " Two slash rounded with decimals because numerator value in decmial format")
+print(11//4.0, " Two slash rounded with decimals because denominator value provides float")
+print(4.3//2, "neareset decimal")
 
-print(6//4) # division with no float output rounded values
-print(6/4) # division with no float output in decimals
 
-# Percent symbol
-print (6%3) # modulus remainder whereby output is 0
-print (6%4) # modulus remainder whereby output is 2
+# Percent symbol (modulo operator)
+print()
+print (6%3, "modulo operator remainder whereby output is 0")
+print (6%4, "modulo operator remainder whereby output is 2")
+print(12 % 4.5)
 
-#
+#Right sided binding example
+print()
+print(2 ** 2 ** 3)
+
+# Examples
+print()
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2) # output is 10.0
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4)) # -0.5 0.5 0 -1
+print((2 % -4), (2 % 4), (2 ** 3 ** 2)) # -2 2 512
