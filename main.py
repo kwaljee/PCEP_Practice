42273eb74e811693066c6438d914f2e03f3dfbd7
Karim <kwaljee@gmail.com>
2021-05-30 15:43:30 -0400

    Removed comments and updated sections with comments

--- main.py
+++ main.py
@@ -1,56 +1,55 @@
-# Module 2 print() function with sep and end
+# Module 2: print() function with sep and end
 
+first_name = input("What is your first name?\n")
+print("Hello,", first_name, "it's nice to meet you!", sep=" ", end="\n\n")
 
-#first_name = input("What is your first name?\n")
-#print("Hello,", first_name, "it's nice to meet you!", sep=" ")
-
-
-# print("    *" * 2)
-# print("   * *" * 2)
-# print("  *   *" * 2)
-# print(" *     *" * 2)
-# print("***   ***" * 2)
-# print("  *   *" * 2)
-# print("  *   *" * 2)
-# print("  *****" * 2)
+# string multiplication
+print("    *" * 2)
+print("   * *" * 2)
+print("  *   *" * 2)
+print(" *     *" * 2)
+print("***   ***" * 2)
+print("  *   *" * 2)
+print("  *   *" * 2)
+print("  *****" * 2)
 
 
 # data types in python aka python literals
 
 # string
-# a = "2"
-# print(type(a))
+a = "2"
+print(type(a))
 
-# # integer
-# b = 2
-# print(type(b))
-#
-# # float
-# c = 3.0
-# print(type(c))
-#
-# # coding floats
-# c2 = 6.62607E-34
-# print(c2)
-# print(type(c2))
-#
-# c3 = 0.00000001
-# print(c3)
-# print(type(c3))
-#
-# # boolean
-# d = False
-# print(type(d))
-#
-# # octal
-# e = 0o123
-# print(e)
-# print(type(e))
-#
-# # hexidecmial
-# f = 0x123
-# print(f)
-# print(type(e))
+# integer
+b = 2
+print(type(b))
+
+# float
+c = 3.0
+print(type(c))
+
+# coding floats
+c2 = 6.62607E-34
+print(c2)
+print(type(c2))
+
+c3 = 0.00000001
+print(c3)
+print(type(c3))
+
+# boolean
+d = False
+print(type(d))
+
+# octal
+e = 0o123
+print(e)
+print(type(e))
+
+# hexidecmial
+f = 0x123
+print(f)
+print(type(e))
 
 
 #Scenario: Write a one-line piece of code, using the print() function, as well as the newline and escape characters, to match the expected result outputted on three lines.
