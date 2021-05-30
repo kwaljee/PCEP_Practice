bad8c1ba65222c17531c4e66ba8526145acbe003
Karim <kwaljee@gmail.com>
2021-05-30 16:00:16 -0400

    Updated names and refactored.

---
+++ Module 2 - 02  Artithmetic.py
@@ -0,0 +1,17 @@
+# Operators
+
+
+# Asterisk
+
+print(2*3) # multiplication
+print(2**3) # exponential multiplication
+
+# Slash
+print(6/2) # division with float output
+print(6//2) # division with no float output
+
+# Percent symbol
+print (6%3) # modulus remainder whereby output is 0
+print (6%4) # modulus remainder whereby output is 2
+
+#
